6f4a82c421dd9d27a3c930bde2cee515584dc83c
Shyam Sunder <shyam.udupa@gmail.com>
2019-07-09 10:58:53 +0530

    added NSE and BSE index constituents

--- R/model-Indices.R
+++ R/model-Indices.R
@@ -21,12 +21,24 @@ Indices <- setRefClass('Indices',
        "Query the index time-series published by the NSE"
 
        return(tbl(.self$conn, 'BHAV_INDEX') %>%
-         select(NAME = INDEX_NAME, TIME_STAMP = TIME_STAMP, HIGH = PX_HIGH, LOW = PX_LOW, OPEN = PX_OPEN, CLOSE = PX_CLOSE, VOLUME = TRD_QTY))
+         select(NAME = INDEX_NAME, TIME_STAMP, HIGH = PX_HIGH, LOW = PX_LOW, OPEN = PX_OPEN, CLOSE = PX_CLOSE, VOLUME = TRD_QTY))
+     },
+     NseConstituents = function(){
+        "Query the latest constituents of NSE indices"
+
+        return(tbl(.self$conn, 'INDEX_NSE_3') %>%
+         select(NAME = INDEX_NAME, TIME_STAMP, SYMBOL, INDUSTRY, CAP_WEIGHT))
      },
      BseTimeSeries = function(){
        "Query the index time-series published by the BSE"
 
        return(tbl(.self$conn, 'BHAV_INDEX_BSE') %>%
-         select(NAME = INDEX_NAME, TIME_STAMP = TIME_STAMP, HIGH = PX_HIGH, LOW = PX_LOW, OPEN = PX_OPEN, CLOSE = PX_CLOSE))
+         select(NAME = INDEX_NAME, TIME_STAMP, HIGH = PX_HIGH, LOW = PX_LOW, OPEN = PX_OPEN, CLOSE = PX_CLOSE))
+     },
+     BseConstituents = function(){
+        "Query the latest constituents of BSE indices"
+
+        return(tbl(.self$conn, 'INDEX_BSE2') %>%
+                  select(NAME = INDEX_NAME, TIME_STAMP = INDEX_DATE, CODE = SECURITY_CODE, SYMBOL = NSE_SYMBOL, SECURITY_NAME))
      }
   ))
