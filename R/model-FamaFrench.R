19f99f5be4bb1c4f9c66e7161bf2d27ebe85a63c
Shyam Sunder <shyam.udupa@gmail.com>
2019-08-08 11:48:50 +0530

    renamed a column in Fama-French dataset

--- R/model-FamaFrench.R
+++ R/model-FamaFrench.R
@@ -20,13 +20,13 @@ FamaFrench <- setRefClass('FamaFrench',
      "Query the Fama-French 5-factor daily returns (http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/Data_Library/f-f_5_factors_2x3.html)"
 
      return(tbl(.self$connUs2, 'FAMA_FRENCH_5_FACTOR_DAILY') %>%
-              select(TIME_STAMP, KEY_ID, DAILY_RETURN))
+              select(TIME_STAMP, KEY_ID, RET))
    },
    Industry49Daily = function(){
      "Query the Fama-French daily returns of 49 different industries (http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/Data_Library/det_49_ind_port.html)"
 
      return(tbl(.self$connUs2, 'FAMA_FRENCH_INDUSTRY_49_DAILY') %>%
-              select(TIME_STAMP, KEY_ID, RET_TYPE, DAILY_RETURN))
+              select(TIME_STAMP, KEY_ID, RET_TYPE, RET))
    },
    MomentumDaily = function(){
      "Query the Fama-French daily returns of momentum factor and portfolios"
@@ -35,7 +35,7 @@ FamaFrench <- setRefClass('FamaFrench',
      #portfolios: https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/Data_Library/det_10_port_form_pr_12_2_daily.html
 
      return(tbl(.self$connUs2, 'FAMA_FRENCH_MOMENTUM_DAILY') %>%
-              select(TIME_STAMP, KEY_ID, RET_TYPE, DAILY_RETURN))
+              select(TIME_STAMP, KEY_ID, RET_TYPE, RET))
    },
    MomentumMonthly = function(){
      "Query the Fama-French monthly returns of momentum factor and portfolios"
@@ -44,7 +44,7 @@ FamaFrench <- setRefClass('FamaFrench',
      #portfolios: https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/Data_Library/det_10_port_form_pr_12_2.html
 
      return(tbl(.self$connUs2, 'FAMA_FRENCH_MOMENTUM_MONTHLY') %>%
-              select(TIME_STAMP, KEY_ID, RET_TYPE, DAILY_RETURN))
+              select(TIME_STAMP, KEY_ID, RET_TYPE, RET))
    }
 
   ))
